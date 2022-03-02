8d9d0f3249c1a0967225e0dadb2c5b214fdb0626
Amanda <97350416+CinnamonSubset@users.noreply.github.com>
2022-03-02 14:20:26 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ytchallangearrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[2];
+
+            Console.WriteLine("Type in stundents names: ");
+
+            for (int i = 0; i < students.Length; i++)       
+            {
+                students[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nHere are the names in alphabetic order: ");
+
+            Array.Sort(students);
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
